34a4fd692da44200aaa65e06d3535c48d6133d05
popcheese9 <popcheese9@gmail.com>
2016-09-12 15:17:19 -0400

    reAdded arithmetic_functions.h
    Hopefully this time this file will be placed correctly.

---
+++ Math%20Checker-Terminal%20Edition/arithmetic_functions.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace add {
+	double a;
+	double b;
+	static void add();
+}
+
+namespace subtract {
+	double a;
+	double b;
+	static void sub();
+}
+
+namespace multiply {
+	double a;
+	double b;
+	static void multiply();
+}
+
+namespace division {
+	double a;
+	double b;
+	static void divide();
+}
